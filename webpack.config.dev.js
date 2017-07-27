6178d05ce10de47fe7cd818e45b099d269aeae9f
Marshall Smith <tmsmith@gmail.com>
2017-07-26 20:18:26 -0700

    Configure webpack for bundling dev environment.

---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+import path from 'path';
+
+export default {
+    devtool: 'inline-source-map',
+    entry: [
+        path.resolve(__dirname, 'src/index')
+    ],
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style', 'css']
+            }
+        ]
+
+    }
+}
